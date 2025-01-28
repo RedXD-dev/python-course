c19091aa459106ce08b358a09abc3e1a0bc4f78f
yayes <yayes@MacBook-Pro-de-David.local>
2025-01-28 15:03:50 -0600

    Este archivo sirve para eliminar duplicados

---
+++ eliminar_duplicados.py
@@ -0,0 +1,21 @@
+"""lista=[]
+contador=0
+lista2=[]
+number=0
+while number!=-1:
+    number=int(input("Ingrese un numero (o escribe'-1' para terminar):"))
+    lista2.append(number)
+    #number = 10
+    for e in lista:#10,20,30
+        print(e)
+        if e == number:
+           contador+=1
+        
+    if contador==0:
+        lista.append(number)
+
+    contador=0
+
+lista2.remove(-1)
+lista.remove(-1)
+print(f"La lista de {lista2} sin duplicados es: {lista}")"""
