4990977046e0572ee27e6fe346523baf10cde841
yayes <yayes@MacBook-Pro-de-David.local>
2025-01-29 15:42:26 -0600

    Se removieron todos los if pero se quedo uno junto al ord

--- alfa_numericos.py
+++ alfa_numericos.py
@@ -2,22 +2,7 @@
 contador=0
 texto=input("ingrese un texto alfa numerico (ejemplo=dearh3w283429):")
 for i in texto:
-    if i=='1':
-        contador+=1
-    elif i=='2':
-        contador+=2
-    elif i=='3':
-        contador+=3
-    elif i=='4':
-        contador+=4
-    elif i=='5':
-        contador+=5
-    elif i=='6':
-        contador+=6
-    elif i=='7':
-        contador+=7
-    elif i=='8':
-        contador+=8
-    elif i=='9':
-        contador+=9
+    if i== '1' or i == '2' or i=='3' or i=='4' or i=='5' or i=='6' or i=='7' or i=='8' or i=='9':
+        contador+=ord(i)-ord("0")
+
 print(f"La suma del texto {texto} en alfa numerico su suma es:{contador}")
