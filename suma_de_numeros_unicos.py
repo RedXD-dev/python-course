6ee5a294ee157037a03b36b126c52dbe8e33b689
yayes <yayes@MacBook-Pro-de-David.local>
2025-02-04 15:02:46 -0600

    Este archivo suma los numeros unicos

---
+++ suma_de_numeros_unicos.py
@@ -0,0 +1,17 @@
+lista=[]
+lista1=[0,0,0,0,0,0,0,0,0,0]
+number=0
+contador=0
+while number!='-1':
+    number=(input("Ingrese un numero '0-9' (o escribe '-1' para terminar):"))
+    lista.append(number)
+lista.remove('-1')
+for a in lista:
+    cosa=ord(a)-ord("0")
+    lista1[cosa]+=1
+for e in lista:
+    cosa=ord(e)-ord("0")
+    if lista1[cosa]==1:
+        contador+=cosa
+print(contador)
+print(lista1)
