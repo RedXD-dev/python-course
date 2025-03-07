fb6102710c3722ee5c16ebbfd5be50cae04afd03
yayes <yayes@MacBook-Pro-de-David.local>
2025-03-07 14:45:07 -0600

    Este archivo se hizo con listas

--- texto_con_lineas.py
+++ texto_con_lineas.py
@@ -2,13 +2,13 @@
 #print(h-o-l-a)
 texto=input("Ingrese un texto: ")
 contador=len(texto)-1
+lista=[]
 
 for a in texto:
-    print(a,end="")
-    
+    lista.append(a)
     if contador!=0:
-        print("-",end="")
-
+        lista.append("-")
     contador-=1
-
-print("")
+for e in lista:
+    print(e,end="")
+#h-o-l-a
