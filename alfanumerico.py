d8ab316813cbb5dfcbd8dc94288a9b6eef5c786c
yayes <yayes@MacBook-Pro-de-David.local>
2025-03-11 14:55:50 -0600

    Este archivo cuenta ahora las mayusculas

--- alfanumerico.py
+++ alfanumerico.py
@@ -1,6 +1,8 @@
 texto=input("Ingrese un texto alfanumerico:")
 for a in texto:
-    contador=ord(a)-ord("a")
+    contador=ord(a)-ord("A")
     if contador>=0 and contador<=25:
+        print(a,end="")
+    elif contador>=32 and contador<=57:
         print(a,end="")
 print()
