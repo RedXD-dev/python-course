27b7aca1afbc9070015bf4437185dd0db5639d6c
yayes <yayes@MacBook-Pro-de-David.local>
2025-03-18 15:02:03 -0600

    Este archivo tiene un error

--- carreras.py
+++ carreras.py
@@ -8,27 +8,34 @@
 #Imprime=0
 contador=-1
 carrito=0
-Longitud=input("Ingrese la longitud de la pista: ")
-lista=["🚗"]
-for a in range(int(Longitud)):
+Longitud=int(input("Ingrese la longitud de la pista: "))
+lista=["🚘"]
+for a in range(Longitud):
     lista.append("_")
-while carrito < int(Longitud):
+while carrito < Longitud:        
+    print(carrito)
+    print(lista)
+    print(Longitud)
+    print(len(lista))
+
     for e in lista:
-        print(carrito)
-        print(lista)
-        print(Longitud)
         cosa=input("Ingrese 'f' para moverse hacia delante y 'b' para retroceder: ")
         if cosa=="f":
             carrito+=1
             lista.insert(0,"_")
             lista.pop()
+            if carrito <= int(Longitud):
+                lista.pop(carrito)
+                lista.insert(carrito,"🚘")
         elif cosa=="b":
             if carrito>=1:
                 carrito-=1
                 lista.pop(0)
                 lista.append("_")
+                lista.pop(carrito)
+                lista.insert(carrito,"🚗")
             else:
                 print("Tu carrito no puede retroceder")
                 contador-=1
         contador+=1
-print(f"Tu 🚗 le tomo {contador} intentos en llegar al final")
+print(f"Tu 🚘 le tomo {contador} intentos en llegar al final")
