b2af67cf7506e03c83e9e8c8ac7637bf9336776d
yayes <yayes@MacBook-Pro-de-David.local>
2025-03-18 15:13:53 -0600

    Este archivo corrige los errores anteriores

--- carreras.py
+++ carreras.py
@@ -12,30 +12,25 @@ Longitud=int(input("Ingrese la longitud de la pista: "))
 lista=["🚘"]
 for a in range(Longitud):
     lista.append("_")
-while carrito < Longitud:        
-    print(carrito)
+while carrito <= Longitud:
     print(lista)
-    print(Longitud)
-    print(len(lista))
-
-    for e in lista:
-        cosa=input("Ingrese 'f' para moverse hacia delante y 'b' para retroceder: ")
-        if cosa=="f":
-            carrito+=1
-            lista.insert(0,"_")
-            lista.pop()
-            if carrito <= int(Longitud):
-                lista.pop(carrito)
-                lista.insert(carrito,"🚘")
-        elif cosa=="b":
-            if carrito>=1:
-                carrito-=1
-                lista.pop(0)
-                lista.append("_")
-                lista.pop(carrito)
-                lista.insert(carrito,"🚗")
-            else:
-                print("Tu carrito no puede retroceder")
-                contador-=1
-        contador+=1
+    cosa=input("Ingrese 'f' para moverse hacia delante y 'b' para retroceder: ")
+    if cosa=="f":
+        carrito+=1
+        lista.insert(0,"_")
+        lista.pop()
+        if carrito <= int(Longitud):
+            lista.pop(carrito)
+            lista.insert(carrito,"🚘")
+    elif cosa=="b":
+        if carrito>=1:
+            carrito-=1
+            lista.pop(0)
+            lista.append("_")
+            lista.pop(carrito)
+            lista.insert(carrito,"🚗")
+        else:
+            print("Tu carrito no puede retroceder")
+            contador-=1
+    contador+=1
 print(f"Tu 🚘 le tomo {contador} intentos en llegar al final")
