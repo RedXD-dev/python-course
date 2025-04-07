49a4c3215e7e5e555db9c1e01efbafb820eb0cab
yayes <yayes@MacBook-Pro-de-David.local>
2025-04-07 14:45:04 -0600

    Este archivo imprime las diagonales de un sentido

---
+++ diagonal_1_matrices.py
@@ -0,0 +1,12 @@
+matris=[
+    [1 ,2 ,3 ,4 ,5 ],
+    [6 ,7 ,8 ,9 ,10],
+    [11,12,13,14,15],
+    [16,17,18,19,20],
+    [21,22,23,24,25]
+]
+fila = 5
+contador = 0
+for a in range(0,fila):
+   print(f"fila {contador}: {matris[a] [contador]}")
+   contador+=1
