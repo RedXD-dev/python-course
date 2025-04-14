364427663469e0310e547f1d8679788c66c1e1a0
yayes <yayes@MacBook-Pro-de-David.local>
2025-04-14 14:17:20 -0600

    Mapa con obstaculos con contador matrices

--- obstaculos.py
+++ obstaculos.py
@@ -10,7 +10,10 @@ columna=0
 fila_futura=0
 columna_futura=0
 lugar=""
-while lugar!="fin":
+contador=-1
+contador_futuro=-1
+contador_normal=0
+while lugar != repr("fin"):
     print(matrices[0])
     print(matrices[1])
     print(matrices[2])
@@ -24,33 +27,38 @@ while lugar!="fin":
     ["_","_","_","♖","_"],
     ["_","♖","_","_","_"]
     ]
-    lugar=input("Ingrese 'l' = ← o 'u' = ↑ o 'r' = → o 'b' = ↓ o 'fin' para terminar: ")
-    if lugar=="l":
+    lugar=repr(input("Ingrese '⬅' = ← o '⬆' = ↑ o '⮕' = → o '⬇' = ↓ o 'fin' para terminar: "))
+    if (lugar == repr('\x1b[D')):
         if columna_futura!=0:
             columna_futura-=1
         else:
             print("Por aqui no se puede pasar, intenta ir a otro lado")
-    elif lugar=="u":
+    elif (lugar == repr('\x1b[A')):
         if fila_futura!=0:
             fila_futura-=1
         else:
             print("Por aqui no se puede pasar, intenta ir a otro lado")
-    elif lugar=="b":
+    elif (lugar == repr('\x1b[B')):
         if fila_futura!=4:
             fila_futura+=1
         else:
             print("Por aqui no se puede pasar, intenta ir a otro lado")
-    elif lugar=="r":
+    elif (lugar == repr('\x1b[C')):
         if columna_futura!=4:
             columna_futura+=1
         else:
             print("Por aqui no se puede pasar, intenta ir a otro lado")
-
+    contador_futuro+=1
     if matrices[fila_futura][columna_futura]!= "♖":
         fila=fila_futura
         columna=columna_futura
+        contador_normal=contador_futuro
     else:
         fila_futura=fila
         columna_futura=columna
+        contador_futuro=contador_normal
         print("Por aqui no se puede pasar, intenta ir a otro lado")
     matrices[fila][columna]="♟"
+    contador+=1
+print(f"Te moviste {contador} veces")
+print(f"Te moviste sin chocar {contador_normal} veces")
