210a484ddbe37bfa916c30c3e565fbb47dbe6aca
yayes <yayes@MacBook-Pro-de-David.local>
2025-04-14 13:30:58 -0600

    Mapa con obstaculos sin contador de movimientos con matrices

--- obstaculos.py
+++ obstaculos.py
@@ -1,156 +1,56 @@
-#Se tendran 5 listas de tamaño 5
-#ahora en las listas van a ver obstaculos
-fila_0=["♟","⛫","_","_","_"]
-fila_1=["_","_","_","⛫","_"]
-fila_2=["⛫","_","_","_","_"]
-fila_3=["_","_","⛫","_","_"]
-fila_4=["_","_","_","_","⛫"]
+matrices=[
+    ["♟","_","♖","_","_"],
+    ["_","_","_","_","♖"],
+    ["♖","_","_","_","_"],
+    ["_","_","_","♖","_"],
+    ["_","♖","_","_","_"]
+]
 fila=0
 columna=0
 fila_futura=0
 columna_futura=0
-cosa=""
-while cosa!="fin":
-    print(fila_0)
-    print(fila_1)
-    print(fila_2)
-    print(fila_3)
-    print(fila_4)
-    fila_0=["_","⛫","_","_","_"]
-    fila_1=["_","_","_","⛫","_"]
-    fila_2=["⛫","_","_","_","_"]
-    fila_3=["_","_","⛫","_","_"]
-    fila_4=["_","_","_","_","⛫"]
-    cosa=input("Ingrese 'l' = ⬅ o 'u' = ⬆ o 'b' = ⬇  o 'r' = ⮕ o ingrese 'fin' para terminar: ")
-    if cosa=="l":
+lugar=""
+while lugar!="fin":
+    print(matrices[0])
+    print(matrices[1])
+    print(matrices[2])
+    print(matrices[3])
+    print(matrices[4])
+    print(fila_futura,columna_futura)
+    matrices=[
+    ["_","_","♖","_","_"],
+    ["_","_","_","_","♖"],
+    ["♖","_","_","_","_"],
+    ["_","_","_","♖","_"],
+    ["_","♖","_","_","_"]
+    ]
+    lugar=input("Ingrese 'l' = ← o 'u' = ↑ o 'r' = → o 'b' = ↓ o 'fin' para terminar: ")
+    if lugar=="l":
         if columna_futura!=0:
             columna_futura-=1
         else:
-            print("No se permite pasar por aqui")
-    elif cosa=="u":
+            print("Por aqui no se puede pasar, intenta ir a otro lado")
+    elif lugar=="u":
         if fila_futura!=0:
             fila_futura-=1
         else:
-            print("No se permite pasar por aqui")
-    elif cosa=="b":
+            print("Por aqui no se puede pasar, intenta ir a otro lado")
+    elif lugar=="b":
         if fila_futura!=4:
             fila_futura+=1
         else:
-            print("No se permite pasar por aqui")
-    elif cosa=="r":
+            print("Por aqui no se puede pasar, intenta ir a otro lado")
+    elif lugar=="r":
         if columna_futura!=4:
             columna_futura+=1
         else:
-            print("No se permite pasar por aqui")
+            print("Por aqui no se puede pasar, intenta ir a otro lado")
 
-    if fila_futura==0:
-
-        if fila_0[columna_futura] == "⛫":
-            print("no se puede pasar por aqui")
-            fila_futura = fila
-            columna_futura = columna
-            if fila==0:
-                fila_0[columna] = "♟"
-            elif fila==1:
-                fila_1[columna] = "♟"
-            elif fila==2:
-                fila_2[columna] = "♟"
-            elif fila==3:
-                fila_3[columna] = "♟"
-            elif fila==4:
-                fila_4[columna] = "♟"
-            
-
-        else:
-            fila = fila_futura
-            columna = columna_futura
-            fila_0[columna] = "♟"
-
-    elif fila_futura==1:
-
-        if fila_1[columna_futura] == "⛫":
-            print("no se puede pasar por aqui")
-            fila_futura = fila
-            columna_futura = columna
-            if fila==0:
-                fila_0[columna] = "♟"
-            elif fila==1:
-                fila_1[columna] = "♟"
-            elif fila==2:
-                fila_2[columna] = "♟"
-            elif fila==3:
-                fila_3[columna] = "♟"
-            elif fila==4:
-                fila_4[columna] = "♟"
-            
-        else:
-            fila = fila_futura
-            columna = columna_futura
-            fila_1[columna] = "♟"
-
-    elif fila_futura==2:
-
-        if fila_2[columna_futura] == "⛫":
-            print("no se puede pasar por aqui")
-            fila_futura = fila
-            columna_futura = columna 
-            if fila==0:
-                fila_0[columna] = "♟"
-            elif fila==1:
-                fila_1[columna] = "♟"
-            elif fila==2:
-                fila_2[columna] = "♟"
-            elif fila==3:
-                fila_3[columna] = "♟"
-            elif fila==4:
-                fila_4[columna] = "♟"
-
-        else:
-
-            fila = fila_futura
-            columna = columna_futura
-            fila_2[columna] = "♟"
-
-    elif fila_futura==3:
-
-        if fila_3[columna_futura] == "⛫":
-            print("no se puede pasar por aqui")
-            fila_futura = fila
-            columna_futura = columna
-            if fila==0:
-                fila_0[columna] = "♟"
-            elif fila==1:
-                fila_1[columna] = "♟"
-            elif fila==2:
-                fila_2[columna] = "♟"
-            elif fila==3:
-                fila_3[columna] = "♟"
-            elif fila==4:
-                fila_4[columna] = "♟"
-            
-        else:
-            fila = fila_futura
-            columna = columna_futura
-            fila_3[columna] = "♟"
-
-    elif fila_futura==4:
-
-        if fila_4[columna_futura] == "⛫":
-            print("no se puede pasar por aqui")
-            fila_futura = fila
-            columna_futura = columna
-            if fila==0:
-                fila_0[columna] = "♟"
-            elif fila==1:
-                fila_1[columna] = "♟"
-            elif fila==2:
-                fila_2[columna] = "♟"
-            elif fila==3:
-                fila_3[columna] = "♟"
-            elif fila==4:
-                fila_4[columna] = "♟"
-            
-        else:
-            fila = fila_futura
-            columna = columna_futura
-            fila_4[columna] = "♟"
+    if matrices[fila_futura][columna_futura]!= "♖":
+        fila=fila_futura
+        columna=columna_futura
+    else:
+        fila_futura=fila
+        columna_futura=columna
+        print("Por aqui no se puede pasar, intenta ir a otro lado")
+    matrices[fila][columna]="♟"
